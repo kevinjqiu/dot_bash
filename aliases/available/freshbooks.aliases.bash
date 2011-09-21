5b6781c5faa91bf3e80144737660ce1119af56c6
Kevin Qiu <kevin@freshbooks.com>
2011-09-20 23:31:47 -0400

    freshbooks aliases

---
+++ aliases/available/freshbooks.aliases.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# shortcuts for dev
+alias optimus='ssh kevinqiu@optimus.2ndsiteinc.com'
+alias prod='ssh kevinqiu@prod.2ndsiteinc.com'
+alias fa='cd ~/src/freshapp'
+alias ev='cd ~/src/evolve/evolve'
+
+alias freshbooks_vpn='cd ~/.openvpn/freshbooks && sudo openvpn --script-security 3 --config freshbooks.ovpn'
+alias connect_dev='ssh fresh@freshbooksdev.com'
