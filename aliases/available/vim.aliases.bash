bc8341a4cdb425c817f1025808460d6f953dadc2
Kevin Qiu <kevin@freshbooks.com>
2012-04-07 13:26:48 -0400

    --sync for vim

--- aliases/available/vim.aliases.bash
+++ aliases/available/vim.aliases.bash
@@ -1,10 +1,12 @@
 #!/bin/bash
 case $OSTYPE in
     linux*)
-        alias mvim='gvim --sync'
+        # alias mvim='gvim --sync'
+        alias mvim='gvim'
         ;;
 esac
 
+alias vim='vim --sync'
 alias v='vim'
 alias vr='mvim --remote-tab'
 alias vv='mvim'
