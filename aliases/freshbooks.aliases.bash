9ef72c3faaeb046ab85738b2100c9bee0e607f37
Kevin Qiu <kevin@freshbooks.com>
2012-09-12 21:57:50 -0400

    Removed some unused stuff

--- aliases/freshbooks.aliases.bash
+++ aliases/freshbooks.aliases.bash
@@ -4,5 +4,5 @@
 alias optimus='ssh kevinqiu@optimus.2ndsiteinc.com'
 alias prod='ssh kevinqiu@prod.2ndsiteinc.com'
 
-alias freshbooks_vpn='cd ~/.openvpn/freshbooks && sudo openvpn --script-security 3 --config freshbooks.ovpn'
+alias freshbooks_vpn='cd ~/.openvpn/freshbooks && sudo openvpn --script-security 3 --config freshbooks.ovpn; cd -'
 alias connect_dev='ssh fresh@freshbooksdev.com'
