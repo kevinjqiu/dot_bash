1095c3baff3a453ce9489162b663687bb3b7ea31
Kevin Qiu <kevin@freshbooks.com>
2012-11-23 10:11:36 -0500

    Add Evolve aliases

--- aliases/freshbooks.aliases.bash
+++ aliases/freshbooks.aliases.bash
@@ -6,3 +6,6 @@ alias prod='ssh kevinqiu@prod.2ndsiteinc.com'
 
 alias freshbooks_vpn='cd ~/.openvpn/freshbooks && sudo openvpn --script-security 3 --config freshbooks.ovpn; cd -'
 alias connect_dev='ssh fresh@freshbooksdev.com'
+
+alias evenv='source ~/evolve-env/bin/activate'
+alias ev='cd ~/evolve-env && gunicorn_paster evolve.ini'
