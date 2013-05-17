c2bee411143761ddcd9ddfaa481a0b907b72656d
Kevin Qiu <kevin@freshbooks.com>
2013-05-16 23:14:39 -0400

    Update vpn command

--- aliases/freshbooks.aliases.bash
+++ aliases/freshbooks.aliases.bash
@@ -4,5 +4,6 @@
 alias optimus='ssh kevinqiu@optimus.2ndsiteinc.com'
 alias prod='ssh kevinqiu@prod.2ndsiteinc.com'
 
-alias freshbooks_vpn='cd ~/.openvpn/freshbooks && sudo openvpn --script-security 3 --config freshbooks.ovpn; cd -'
+# alias freshbooks_vpn='cd ~/.openvpn/freshbooks && sudo openvpn --script-security 3 --config freshbooks.ovpn; cd -'
+alias freshbooks_vpn='netExtender -u kevin -d 2ndsiteinc vpn.2ndsiteinc.com'
 alias connect_dev='ssh fresh@freshbooksdev.com'
