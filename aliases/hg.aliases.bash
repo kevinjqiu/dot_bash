ee545aa985e8005bd07406383a1a4fcf22f63ed6
Kevin Jing Qiu <kevin@idempotent.ca>
2015-12-07 11:48:29 -0500

    hg aliases

--- aliases/hg.aliases.bash
+++ aliases/hg.aliases.bash
@@ -3,6 +3,7 @@
 alias hs='hg status'
 alias hsum='hg summary'
 alias hcm='hg commit -m'
+alias hpr='hg pull --rebase'
 
 function hg-help() {
   echo "Mercurial Alias Help"
@@ -10,5 +11,6 @@ function hg-help() {
   echo "  hs    = hg status"
   echo "  hsum  = hg summary"
   echo "  hcm   = hg commit -m"
+  echo "  hpr   = hg pull --rebase"
   echo
 }
