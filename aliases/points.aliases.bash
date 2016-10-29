8b372856721ad21ebc428b0e78a2417c528a8714
Kevin Jing Qiu <kevin@idempotent.ca>
2016-10-29 12:24:23 -0400

    Update pointsvpn

--- aliases/points.aliases.bash
+++ aliases/points.aliases.bash
@@ -1,2 +1,2 @@
 #! /bin/bash
-alias ptsvpn='cd ~/.ovpn && sudo openvpn kevin.qiu.ovpn'
+alias ptsvpn='cd ~/.ovpn && sudo openvpn kevin.qiu.ovpn && tac /etc/resolv.conf > /tmp/resolv.conf && echo 10.20.12.1 >> /tmp/resolv.conf && sudo mv /tmp/resolv.conf /etc/resolv.conf'
