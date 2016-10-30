3736aaee3510ca8adab0ce96266a1ad00b8a6ebd
Kevin Jing Qiu <kevin@idempotent.ca>
2016-10-30 10:34:06 -0400

    Update

--- aliases/points.aliases.bash
+++ aliases/points.aliases.bash
@@ -1,7 +1,7 @@
 #! /bin/bash
 function ptsvpn {
-    tac /etc/resolv.conf > /tmp/resolv.conf
-    echo "nameserver 10.20.12.1" >> /tmp/resolv.conf
-    tac /tmp/resolv.conf | sudo tee /etc/resolv.conf
+    #tac /etc/resolv.conf > /tmp/resolv.conf
+    #echo "nameserver 10.20.12.1" >> /tmp/resolv.conf
+    #tac /tmp/resolv.conf | sudo tee /etc/resolv.conf
     cd ~/.ovpn && sudo openvpn kevin.qiu.ovpn
 }
