40e0b5b46ba099a1dcee6d13a2c543cf2bc3ed4f
Kevin Jing Qiu <kevin@idempotent.ca>
2019-01-26 13:02:35 -0500

    kenv function

--- aliases/available/kubernetes.aliases.bash
+++ aliases/available/kubernetes.aliases.bash
@@ -2,16 +2,24 @@ cite 'about-kubernetes'
 about-alias 'common kubernetes abbreviations'
 
 alias k='kubectl'
-alias k8s-dev="export KUBECONFIG=$HOME/.kube/k8s-dev"
-alias k8s-util="export KUBECONFIG=$HOME/.kube/k8s-util"
-alias k8s-prod="export KUBECONFIG=$HOME/.kube/k8s-prod"
 
-#alias kalpha='export KUBECONFIG=$HOME/.kube/alpha'
-#alias kgamma='export KUBECONFIG=$HOME/.kube/kube-config-gamma'
-#alias kdelta='export KUBECONFIG=$HOME/.kube/kube-config-delta'
-#alias kstaging='export KUBECONFIG=$HOME/.kube/kube-config-staging'
-#alias kmonarch='export KUBECONFIG=$HOME/.kube/monarch'
-#alias kminikube='export KUBECONFIG=$HOME/.kube/minikube'
 alias kclear='export KUBECONFIG='
 alias kgp='kubectl get pods'
 alias kdbg='kubectl run -it --rm --restart=Never dbg --image=kevinjqiu/dbg bash'
+
+function kenv {
+    if [[ "$#" != "1" ]]; then
+        echo "Usage: kenv|k8s <envfile>"
+        return
+    fi
+    env=$1
+    kubeconfig_path="$HOME/.kube/$env"
+    if [[ ! -f "$kubeconfig_path" ]]; then
+        echo "kube config file $kubeconfig_path does not exist!"
+        return
+    fi
+    echo "export KUBECONFIG=$kubeconfig_path"
+    export KUBECONFIG=$kubeconfig_path
+}
+
+alias k8s='kenv'
