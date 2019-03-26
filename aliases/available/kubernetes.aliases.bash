564e6b34381830822e512880f8daceba345b34fd
Kevin Jing Qiu <kevin@idempotent.ca>
2019-03-26 01:03:43 -0400

    Update kdbg

--- aliases/available/kubernetes.aliases.bash
+++ aliases/available/kubernetes.aliases.bash
@@ -5,7 +5,7 @@ alias k='kubectl'
 
 alias kclear='export KUBECONFIG='
 alias kgp='kubectl get pods'
-alias kdbg='kubectl run -it --rm --restart=Never dbg --image=kevinjqiu/dbg bash'
+alias kdbg='krun kevinjqiu/dbg bash'
 
 function krun {
     image=$1
